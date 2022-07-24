7b229bd18df78ca0510773b14e8aed810ba0534b
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:35 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,11 @@
-var CACHE_NAME = "2022-05-22 07:30";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/siminym-ja/",
   "/siminym-ja/index.js",
   "/siminym-ja/sql.js-httpvfs/sql-wasm.wasm",
   "/siminym-ja/sql.js-httpvfs/sqlite.worker.js",
   "/siminym-ja/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
