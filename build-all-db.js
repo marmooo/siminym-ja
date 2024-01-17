c1f2b419ac3b04de410bf5cc76134e0cf5dd35fe
marmooo <marmooo@users.noreply.github.com>
2024-01-17 10:37:01 +0900

    fix

--- build-all-db.js
+++ build-all-db.js
@@ -5,5 +5,4 @@ for (const size of sizes) {
   await $`bash build-dict.sh ${size}`;
   await $`bash build-db.sh ${size}`;
 }
-Deno.removeSync("docs/db/80000", { recursive: true });
 Deno.renameSync("docs/db/1000000", "docs/db/80000");
