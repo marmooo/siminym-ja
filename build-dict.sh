99550192a64da126bdf3672bfbbd249833e17e1f
marmooo <marmooo@users.noreply.github.com>
2025-04-01 00:22:00 +0900

    update deps

--- build-dict.sh
+++ build-dict.sh
@@ -1,5 +1,5 @@
-python reduce.py $1
-python -m pymagnitude.converter \
+rye run python reduce.py $1
+rye run python -m pymagnitude.converter \
   -i cc.ja.300-small.vec \
   -o cc.ja.300-small.magnitude
-python similars.py
+rye run python similars.py
